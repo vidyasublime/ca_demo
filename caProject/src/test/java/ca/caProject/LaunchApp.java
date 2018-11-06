a145dad53adcdf4ef37c9908cf04390b60b85a28
Afreen <info@sublimetechnocorp.com>
2018-11-06 17:06:48 +0530

    secibd

--- caProject/src/test/java/ca/caProject/LaunchApp.java
+++ caProject/src/test/java/ca/caProject/LaunchApp.java
@@ -10,18 +10,9 @@ import ca.caProject.Constants;
 import pageObjects.LoginPage;
 
 public class LaunchApp {
-	public static WebDriver driver = null;
-	private String title1="";
-  @Test(priority=0,description="Login Page")
-  public void Login() throws Exception {
 	
-	  
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Android4\\Downloads\\chromedriver.exe");
-		 driver=new ChromeDriver();
-		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//driver.get(Constant.URL);
+	private String title1="";
+ 
 
 
-
-
-  }}
+  }

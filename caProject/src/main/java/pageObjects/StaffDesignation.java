2d126f0b029d3d0e55d53ba584c0d2ea28cea8d9
Afreen <info@sublimetechnocorp.com>
2018-11-06 18:02:27 +0530

    Staff designation

---
+++ caProject/src/main/java/pageObjects/StaffDesignation.java
@@ -0,0 +1,37 @@
+package pageObjects;
+
+public class StaffDesignation {
+
+	private static WebElement element = null;
+	 public static WebElement txtbx_designation_name(WebDriver driver){
+		 
+       element = driver.findElement(By.name("designation_name"));
+
+       return element;
+
+       }
+
+   public static WebElement btn_Submit(WebDriver driver){
+
+       element = driver.findElement(By.xpath("//*[@id=\"add_price_type_form\"]/div[2]/button[1]"));
+        
+       return element;
+
+       }
+   public static WebElement btn_ Reset(WebDriver driver){
+
+       element = driver.findElement(By.xpath("//*[@id=\"add_price_type_form\"]/div[2]/button[2]"));
+        
+       return element;
+
+       }
+   public static WebElement btn_Close(WebDriver driver){
+
+       element = driver.findElement(By.xpath("//*[@id=\"add_price_type_form\"]/div[2]/button[3]"));
+        
+       return element;
+
+       }
+
+	
+}

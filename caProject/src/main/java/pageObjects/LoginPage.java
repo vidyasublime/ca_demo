4ceab5ba09bef8fb294e2d94733e36ad17498431
Afreen <info@sublimetechnocorp.com>
2018-11-06 17:33:40 +0530

    Added Login page

--- caProject/src/main/java/pageObjects/LoginPage.java
+++ caProject/src/main/java/pageObjects/LoginPage.java
@@ -1,5 +1,35 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+	
+	 private static WebElement element = null;
+	 public static WebElement txtbx_UserName(WebDriver driver){
+		 
+         element = driver.findElement(By.id("email"));
+ 
+         return element;
+ 
+         }
+ 
+     public static WebElement txtbx_Password(WebDriver driver){
+ 
+         element = driver.findElement(By.name("password"));
+ 
+         return element;
+ 
+         }
+ 
+     public static WebElement btn_LogIn(WebDriver driver){
+ 
+         element = driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div/form/div[4]/div/div/div/button"));
+          
+         return element;
+ 
+         }
+
 
 }
--- caProject/src/test/java/ca/caProject/LaunchApp.java
+++ caProject/src/test/java/ca/caProject/LaunchApp.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import ca.caProject.Constants;
-import pageObjects.LoginPage;
+
+
 
 public class LaunchApp {
 	

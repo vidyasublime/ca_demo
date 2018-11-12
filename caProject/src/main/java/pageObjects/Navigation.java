7848ce36aa885c316334d4b51b688e59502b9514
Afreen <info@sublimetechnocorp.com>
2018-11-12 11:31:11 +0530

    Navigation

---
+++ caProject/src/main/java/pageObjects/Navigation.java
@@ -0,0 +1,116 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Navigation {
+	
+	private static WebElement element = null;
+	 public static WebElement txtbx_dashboard(WebDriver driver){
+		 
+		 element = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div[2]/ul/li[1]/a"));
+	        
+	       return element;
+
+}
+ public static WebElement txtbx_Assign_task(WebDriver driver){
+		 
+		 element = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div[2]/ul/li[2]/a"));
+	        
+	       return element;
+
+
+	       }
+ public static WebElement txtbx_Master(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div[2]/ul/li[3]/a"));
+        
+       return element;
+
+
+       }
+public static WebElement txtbx_Client(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[2]/div[1]/div/div[2]/h2"));
+        
+       return element;
+
+
+       }
+public static WebElement txtbx_Client_Status(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/h2/a"));
+       
+      return element;
+
+
+      }
+public static WebElement txtbx_Client_Industry(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[2]/div[3]/div/div[2]/h2/a"));
+      
+     return element;
+
+
+     }
+public static WebElement txtbx_Staff(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[3]/div[1]/div/div[2]/h2/a"));
+     
+    return element;
+
+
+    }
+public static WebElement txtbx_Staff_Qualification (WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[3]/div[2]/div/div[2]/h2/a"));
+    
+   return element;
+
+
+   }
+public static WebElement txtbx_Staff_Designation(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[3]/div[3]/div/div[2]/h2/a"));
+   
+  return element;
+
+
+  }
+public static WebElement txtbx_Service(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[4]/div[1]/div/div[2]/h2/a"));
+  
+ return element;
+
+
+ }
+public static WebElement txtbx_Activity(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[4]/div[2]/div/div[2]/h2/a"));
+ 
+return element;
+
+
+}
+public static WebElement txtbx_File_Name_Master (WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/div[2]/div/div/div[4]/div[3]/div/div[2]/h2/a"));
+
+return element;
+
+
+}
+
+ public static WebElement txtbx_Staff_Chat(WebDriver driver){
+	 
+	 element = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div[2]/ul/li[4]/a"));
+        
+       return element;
+
+
+       }
+ 
+ 
+}
--- caProject/src/main/java/pageObjects/StaffDesignation.java
+++ caProject/src/main/java/pageObjects/StaffDesignation.java
@@ -1,5 +1,9 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class StaffDesignation {
 
 	private static WebElement element = null;
